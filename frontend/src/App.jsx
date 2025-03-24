3fd2727334cac4ee464fefcaf6a58e8971c3b2f5
Dhruva <dhruvatej6565@gmail.com>
2025-03-25 00:31:05 +0530

    Updated About

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './componets/Navbar/Navbar'
 import Hero from './componets/Hero/Hero'
+import About from './componets/About/About'
 
 const App = () => {
   return (
     <>
     <Navbar />
     <Hero />
+    <About />
     </>
   )
 }
---
+++ frontend/src/componets/About/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./about.css"
+import themePattern from "../../assets/theme_pattern.svg"
+import Profile from "../../assets/about_profile.svg"
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+            <img src={themePattern} alt="theme" />
+        </div>
+        <div className="about-sections">
+            <div className="about-left">
+                <img src={Profile} alt="profile_img" />
+            </div>
+            <div className="about-right">
+                <div className="about-para">
+                    <p>Hey there! I’m Dhruva Teja D R, a web developer and AI enthusiast with a strong passion for building smart, scalable, and efficient applications. I specialize in backend development using Django, and I’ve also worked with Node.js, React.js, and AI/ML technologies.</p>
+                    <p>With experience in full-stack development, I’ve contributed to projects ranging from AI-driven solutions to real-time web applications. I’ve developed systems like Farmfleet (farm management system), MediDocs (automated patient documentation), and a chat app for Souloxy. Currently, I’m working on an AI-powered Random Question Paper Generator and exploring Langchain, TensorFlow, and LLMs.</p>
+                    <p>Beyond coding, I love documenting my learnings, sketching mandalas, and exploring the intersection of AI and creativity. I’m always up for collaborations on AI and full-stack projects—let’s build something amazing together!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
